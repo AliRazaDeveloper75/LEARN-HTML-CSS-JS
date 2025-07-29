2a67996dc4b88e5014d7c78bb2fdb016b8b63899
AliRazaDeveloper75 <alirazadeveloper75@gmail.com>
2025-07-29 12:59:33 +0500

    ema

---
+++ script.js
@@ -0,0 +1,12 @@
+function downloadResume() {
+  const element = document.getElementById('resume');
+  const opt = {
+    margin:       0.5,
+    filename:     'Ali_Raza_Resume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2 },
+    jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+  };
+
+  html2pdf().set(opt).from(element).save();
+}
